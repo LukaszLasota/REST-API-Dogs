fa2b9f57dc5531e639229b444c09176bf1e78c89
Lukasz Lasota <lukaszlasota89r@gmail.com>
2020-09-11 18:45:48 +0200

    Changes in scss, index, spi.js files

--- js/api.js
+++ js/api.js
@@ -1,62 +1,103 @@
-console.log(44);
-
-// fetch('https://www.googleapis.com/books/v1/volumes?q=quilting')
-//     .then(resp => {
-         
-//         return resp.json();
-//     })
-//     .then(booksInfo =>{
-//         console.log(booksInfo);
-//     })
-//     .catch(err =>{
-//         console.log('Niestety blad', err);
-//     })
 
 class Doggo {
+    constructor() {
+        this.apiUrl = 'https://dog.ceo/api';
+        this.imgEl = document.querySelector('.featured-dog img');
+        this.backgroundEl = document.querySelector('.featured-dog__background');
+        this.tilesEl = document.querySelector('.tiles');
+        this.spinnerEl = document.querySelector('.spinner');
 
-constructor(){
-    this.apiUrl = 'https://dog.ceo/api/';
-    this.imgEl = document.querySelector('.featured-dogs img'); 
-    
-    this.init();
+        this.init();
+    }
+
+    showLoading() {
+        this.spinnerEl.classList.add('spinner--visible');
+    }
+
+    hideLoading() {
+        this.spinnerEl.classList.remove('spinner--visible');
+    }
+
+    listBreeds() {
+        return fetch(`${this.apiUrl}/breeds/list/all`)
+            .then(resp => resp.json())
+            .then(data => data.message);
+    }
+
+    getRandomImage() {
+        return fetch(`${this.apiUrl}/breeds/image/random`)
+            .then(resp => resp.json())
+            .then(data => data.message);
+    }
+
+    getRandomImageByBreed(breed) {
+        return fetch(`${this.apiUrl}/breed/${breed}/images/random`)
+            .then(resp => resp.json())
+            .then(data => data.message);
+    }
+
+    init() {
+        this.showLoading();
+        this.getRandomImage()
+            .then(img => this.showImageWhenReady(img));
+
+        this.showAllBreeds();
+    }
+
+    showImageWhenReady(image) {
+        this.imgEl.setAttribute('src', image);
+        this.backgroundEl.style.background = `url("${image}")`;
+        this.hideLoading();
+    }
+
+    addBreed(breed, subBreed) {
+        let name;
+        let type;
+
+        if (typeof subBreed === 'undefined') {
+            name = breed;
+            type = breed;
+        } else {
+            name = `${breed} ${subBreed}`;
+            type = `${breed}/${subBreed}`;
+        }
+
+        const tile = document.createElement('div');
+        tile.classList.add('tiles__tile');
+
+        const tileContent = document.createElement('div');
+        tileContent.classList.add('tiles__tile-content');
+
+        tileContent.innerText = name;
+        tileContent.addEventListener('click', () => {
+            window.scrollTo(0, 0);
+            this.showLoading();
+            this.getRandomImageByBreed(type)
+                .then(img => this.showImageWhenReady(img));
+        });
+
+        tile.appendChild(tileContent);
+        this.tilesEl.appendChild(tile);
+    }
+
+
+    showAllBreeds() {
+        this.listBreeds()
+            .then(breeds => {
+                for (const breed in breeds) {
+                    if (breeds[breed].length === 0) {
+                        this.addBreed(breed);
+                    } else {
+                        for (const subBreed of breeds[breed]) {
+                            this.addBreed(breed, subBreed);
+                        }
+                    }
+                }
+            });
+    }
+
 }
 
-listBreeds(){
-   return fetch(`${this.apiUrl}/breeds/list/all`)
-    .then(resp => resp.json())
-    .then(data => {
-        return data.message;
+document.addEventListener('DOMContentLoaded', () => {
+        new Doggo();
     });
-}
-
-getRandomImage(){
-    return fetch(`${this.apiUrl}/breeds/image/random`)
-    .then(resp => resp.json())
-    .then(data =>  data.message);
-}
-
-getRandomImageByBreed(breed){
-    return fetch(`${this.apiUrl}/breed/${breed}/images/random`)
-    .then(resp => resp.json())
-    .then(data =>  data.message);
-}
-
-init(){
-    this.getRandomImage()
-        .then(src => this.imgEl.setAttribute('src', src));
-
-    this.listBreeds()
-        .then(breeds => console.log(breeds));
-
-}
-    
-}
-
-document.addEventListener('DOMContentLoaded', ()=>{
-    new Doggo
-})
-
-// const img = document.querySelector('img')
-
-// getRandomImageByBreed('dachshund')
-// .then(imgSrc => img.setAttribute('src', imgSrc))
